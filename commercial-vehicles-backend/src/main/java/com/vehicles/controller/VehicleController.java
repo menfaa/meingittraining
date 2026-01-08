b2a5843b959daf1f5b09087ad17a020aa8971454
Menfaa <U0554@in-tech.global>
2026-01-08 16:00:54 +0100

    Intial commit

---
+++ commercial-vehicles-backend/src/main/java/com/vehicles/controller/VehicleController.java
@@ -0,0 +1,30 @@
+package com.vehicles.controller;
+
+import com.vehicles.model.Vehicle;
+import com.vehicles.service.VehicleService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/vehicles")
+public class VehicleController {
+    private final VehicleService vehicleService;
+
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    @GetMapping
+    public Iterable<Vehicle> getAllVehicles() {
+        return vehicleService.getAllVehicles();
+    }
+
+    @PostMapping
+    public Vehicle createVehicle(@RequestBody Vehicle vehicle) {
+        return vehicleService.saveVehicle(vehicle);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteVehicle(@PathVariable Long id) {
+        vehicleService.deleteVehicle(id);
+    }
+}
---
+++ commercial-vehicles-backend/src/main/java/com/vehicles/service/VehicleService.java
@@ -0,0 +1,26 @@
+package com.vehicles.service;
+
+import com.vehicles.model.Vehicle;
+import com.vehicles.repository.VehicleRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VehicleService {
+    private final VehicleRepository vehicleRepository;
+
+    public VehicleService(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+
+    public Iterable<Vehicle> getAllVehicles() {
+        return vehicleRepository.findAll();
+    }
+
+    public Vehicle saveVehicle(Vehicle vehicle) {
+        return vehicleRepository.save(vehicle);
+    }
+
+    public void deleteVehicle(Long id) {
+        vehicleRepository.deleteById(id);
+    }
+}
